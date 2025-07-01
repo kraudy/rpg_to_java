ea67a4ee4735e590f887bc49fbc55eac9474080c
Roberto Torrez <robkraudy@gmail.com>
2025-07-01 06:25:32 -0600

    Update cmdcall logic
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- source/CmdCall.java
+++ source/CmdCall.java
@@ -5,17 +5,16 @@ public class CmdCall {
     // Like other Java classes, IBM Toolbox for Java classes
     // throw exceptions when something goes wrong. These must
     // be caught by programs that use IBM Toolbox for Java.
+    AS400 system = new AS400();
+    CommandCall command = new CommandCall(system);
     try{
-      AS400 system = new AS400();
-      CommandCall cc = new CommandCall(system);
-    
-      //cc.run("CRTLIB MYLIB");
-      //cc.run("DSPLIBL");
-      cc.run("CHGCURLIB CURLIB(ROBKRAUDY2)");
-      cc.run("CRTSRCPF FILE(ROBKRAUDY2/JAVASRC)");
-      AS400Message[] ml = cc.getMessageList(); 
-      for (int i=0; i<ml.length; i++){
-        System.out.println(ml[i].getText());
+      if(!command.run("CHGCURLIB CURLIB(ROBKRAUDY2)")){
+        System.out.println("Command failed!");
+      }
+      
+      AS400Message[] messageList = command.getMessageList(); 
+      for (int i=0; i<messageList.length; i++){
+        System.out.println(messageList[i].getText());
       }
     }
     catch (Exception e){
