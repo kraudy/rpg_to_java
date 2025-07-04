3216d31e4eee58e60cc3ebe2028e79df9bf9f726
Roberto Torrez <robkraudy@gmail.com>
2025-07-04 06:47:49 -0600

    Add catching exceptions
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- source/TestEnvironmentVariable.java
+++ source/TestEnvironmentVariable.java
@@ -1,3 +1,6 @@
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+
 import com.ibm.as400.access.*;
 
 public class TestEnvironmentVariable {
@@ -11,11 +14,22 @@ public class TestEnvironmentVariable {
     } catch (ObjectDoesNotExistException e) {
       // Try adding the env var here with the cmd object
       System.out.println("Environment variable 'PATH' does not exist on the system.");
-      if(!cmd.run("ADDENVVAR ENVVAR(PATH) VALUE('/home/robrkaudy') LEVEL(*JOB)")){
-        System.out.println("Could not add envvar PATH to job");
+      try {
+        if(!cmd.run("ADDENVVAR ENVVAR(PATH) VALUE('/home/robrkaudy') LEVEL(*JOB)")){
+          System.out.println("Could not add envvar PATH to job");
+        }
+      } catch (PropertyVetoException | AS400SecurityException | ErrorCompletingRequestException | IOException | InterruptedException e2) {
+        e2.printStackTrace();
       }
-      System.out.println(path.getValue());
-    }catch (Exception e){
+
+      try {
+        System.out.println(path.getValue());
+      }catch (AS400SecurityException | ErrorCompletingRequestException | InterruptedException | IOException | ObjectDoesNotExistException e3) {
+        e3.printStackTrace();
+      }  
+    } catch (AS400SecurityException e4){
+      System.out.println("Security exception");
+    } catch (Exception e){
       e.printStackTrace();
     }
 
