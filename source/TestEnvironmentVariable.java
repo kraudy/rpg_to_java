9cf646d32cf07dc4945ad866c0f3ee4ebb406499
Roberto Torrez <robkraudy@gmail.com>
2025-07-04 06:13:25 -0600

    Add env var after exception
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- source/TestEnvironmentVariable.java
+++ source/TestEnvironmentVariable.java
@@ -4,13 +4,17 @@ public class TestEnvironmentVariable {
   public static void main(String... args){
     AS400 system = new AS400();
     EnvironmentVariable path = new EnvironmentVariable(system, "PATH");
+    CommandCall cmd = new CommandCall(system);
 
     try{
       System.out.println(path.getValue());
     } catch (ObjectDoesNotExistException e) {
       // Try adding the env var here with the cmd object
-      System.err.println("Environment variable 'PATH' does not exist on the system.");
-      e.printStackTrace();
+      System.out.println("Environment variable 'PATH' does not exist on the system.");
+      if(!cmd.run("ADDENVVAR ENVVAR(PATH) VALUE('/home/robrkaudy') LEVEL(*JOB)")){
+        System.out.println("Could not add envvar PATH to job");
+      }
+      System.out.println(path.getValue());
     }catch (Exception e){
       e.printStackTrace();
     }
