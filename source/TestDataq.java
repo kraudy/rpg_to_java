a404af033ff35da52eefec0b35b06fe62e8e9c32
Roberto Torrez <robkraudy@gmail.com>
2025-07-05 06:18:22 -0600

    Add dq example
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

---
+++ source/TestDataq.java
@@ -0,0 +1,19 @@
+import com.ibm.as400.access.*;
+
+public class TestDataq {
+  public static void main(String... args){
+    AS400 system = new AS400();
+
+    try {
+      // ObjectDoesNotExistException
+      DataQueue dq = new DataQueue(system, "/QSYS.LIB/ROBKRAUDY2.LIB/MYQUEUE.DTAQ");
+
+      DataQueueEntry dqData = dq.read();
+      byte[] data = dqData.getData();
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+
+    system.disconnectService(AS400.DATAQUEUE);
+  }
+}
