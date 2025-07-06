6d9401365d81bd3cd514c3f015f89678caae4188
Roberto Torrez <robkraudy@gmail.com>
2025-07-05 20:22:55 -0600

    Read json file
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- source/TestReadJson.java
+++ source/TestReadJson.java
@@ -1,5 +1,9 @@
 import com.ibm.as400.access.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Date;
+
 
 public class TestReadJson {
   public static void main(String... args){
@@ -7,28 +11,37 @@ public class TestReadJson {
     //TODO: Add listener
     IFSFile file = new IFSFile(sys, "/home/ROBKRAUDY/notif.json");
 
-    String directory = file.getParent();
-    String name = file.getName();
-    long length = 0;
-    Date date = null;
     try {
-      length = file.length();
-      date = new Date(file.lastModified());
-    } catch (Exception e){
+      if(!file.exists()){
+        System.out.println("File does not exists: " + file.getPath());
+        return;
+      }
+  
+      if(!file.canRead()){
+        System.out.println("Can't read from file: " + file.getPath());
+      }
+    } catch(Exception e){
       e.printStackTrace();
+    }    
+
+    try{
+      IFSFileInputStream fis = new IFSFileInputStream(file);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+
+      // Read the entire file into a StringBuilder
+      StringBuilder jsonContent = new StringBuilder();
+      String line;
+      while ((line = reader.readLine()) != null) {
+          jsonContent.append(line);
+      }
+      
+    } catch (Exception e2){
+      e2.printStackTrace();
     }
 
     // Check if the file exists exists()
     // Get CCSID => getCCSID()
     // Returns storage space available to the user. => getFreeSpace(AS400 system)
 
-    // Check if it can be read
-    if(!file.canRead()){
-      System.out.println("Can't read from file");
-    }
-    
-
-    System.out.println("Dir: " + directory + " Name: " + name + " Length: " + length + " Date: " + date);
-    
   }
 }
