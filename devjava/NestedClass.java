293e043ab2f63e81fb7fdd407df2f6f35ecc25b6
Roberto Torrez <robkraudy@gmail.com>
2025-07-27 09:21:22 -0600

    More class stuff
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- devjava/NestedClass.java
+++ devjava/NestedClass.java
@@ -11,11 +11,36 @@ public class NestedClass{
     }
   }
 
+  static class StaticNestedClass{
+    void accessMembers(NestedClass outer){
+      System.out.println(outer.outerField);
+      System.out.println(staticOuterFiled);
+    }
+  }
+
   public static void main(String... args){
     System.out.println("Inner class:");
     System.out.println("------------");
     NestedClass outerObject = new NestedClass(); 
     NestedClass.InnerClass innerObject = outerObject.new InnerClass();
     innerObject.accessMembers();
+
+    System.out.println("\nStatic nested class:");
+    System.out.println("--------------------");
+    StaticNestedClass staticNestedObject = new StaticNestedClass();
+    staticNestedObject.accessMembers(outerObject);
+
+    System.out.println("\nTop-level class:");
+    System.out.println("--------------------");
+    TopLevelClass topLevelObject = new TopLevelClass();
+    topLevelObject.accessMembers(outerObject);
+
+  }
+}
+
+public class TopLevelClass {
+  void accessMembers(NestedClass outer) {
+      System.out.println(outer.outerField);
+      System.out.println(NestedClass.staticOuterFiled);
   }
 }
