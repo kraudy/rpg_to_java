bf15747a2181f45224102317262c26f230335c2f
Roberto Torrez <robkraudy@gmail.com>
2025-07-29 06:06:40 -0600

    Get net server name
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- ibmi_projects/JavaNetServer/src/main/java/com/example/JavaNetServer.java
+++ ibmi_projects/JavaNetServer/src/main/java/com/example/JavaNetServer.java
@@ -1,9 +1,23 @@
 package com.example;
 
 import com.ibm.as400.access.*;
+import com.ibm.as400.resource.*;
 
 public class JavaNetServer {
-  public static void main( String[] args ){
-    System.out.println( "Hello World!" );
+  public static void main(String... args){
+    AS400 system = new AS400();
+    NetServer ns = new NetServer(system);
+
+    try{
+       // Get the name of the NetServer.
+      System.out.println("Name: " + (String)ns.getAttributeValue(NetServer.NAME));
+
+    }
+    catch (ResourceException e) {
+      e.printStackTrace();
+    }
+    finally {
+      if (system != null) system.disconnectAllServices();
+    }
   }
 }
