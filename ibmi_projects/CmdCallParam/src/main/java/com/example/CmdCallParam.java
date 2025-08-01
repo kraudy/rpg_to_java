918b27c645c9c9abc0c7f22f80ce71fa4d469456
Roberto Torrez <robkraudy@gmail.com>
2025-08-01 07:06:57 -0600

    Excuete command line cmd
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- ibmi_projects/CmdCallParam/src/main/java/com/example/CmdCallParam.java
+++ ibmi_projects/CmdCallParam/src/main/java/com/example/CmdCallParam.java
@@ -6,6 +6,34 @@ import com.ibm.as400.access.*;
 
 public class CmdCallParam {
   public static void main( String... args ){
-    
+    BufferedReader inputStream = new BufferedReader(new InputStreamReader(System.in),1);
+    AS400 system = new AS400();
+
+    String commandStr = null;
+
+    try{
+      System.out.println("Command to execute: ");
+      commandStr = inputStream.readLine();
+
+      CommandCall cmd = new CommandCall(system);
+      if(!cmd.run(commandStr)){
+        System.out.println("Could not execute command");
+        return;
+      }
+
+      AS400Message[] messages = cmd.getMessageList();
+      if(messages.length > 0){
+        for(AS400Message message: messages){
+          System.out.print ( message.getID() );
+          System.out.print ( ": " );
+          System.out.println( message.getText() );
+        }
+      }
+    } catch (Exception e){
+      e.printStackTrace();
+    } finally {
+      if(system != null) system.disconnectAllServices();
+    }
+
   }
 }
