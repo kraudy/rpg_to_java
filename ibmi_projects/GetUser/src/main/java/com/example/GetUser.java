b574cc74095b7aa22798755a9535dd1a75d21fd6
Roberto Torrez <robkraudy@gmail.com>
2025-08-06 20:14:46 -0600

    Get user cur dir
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- ibmi_projects/GetUser/src/main/java/com/example/GetUser.java
+++ ibmi_projects/GetUser/src/main/java/com/example/GetUser.java
@@ -4,6 +4,17 @@ import com.ibm.as400.access.*;
 
 public class GetUser {
   public static void main(String... args){
-    System.out.println( "Hello World!" );
+    AS400 system = new AS400();
+
+    try{
+      // Get current user
+      User user = new User(system, system.getUserId());
+      System.out.println("User dir: " + user.getHomeDirectory());
+
+    } catch (Exception e){
+      e.printStackTrace();
+    } finally {
+      if (system != null) system.disconnectAllServices();
+    }
   }
 }
