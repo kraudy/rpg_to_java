920d71b17675f8d029d4a75de68c51f7d8a01357
Roberto Torrez <robkraudy@gmail.com>
2025-09-04 17:50:17 -0600

    Add compiler project
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

---
+++ ibmi_projects/ObjectCompiler/src/main/java/com/github/kraudy/compiler/ObjectCompiler.java
@@ -0,0 +1,8 @@
+package com.github.kraudy.compiler;
+
+
+public class ObjectCompiler {
+  public static void main( String... args ){
+    System.out.println( "Hello World!" );
+  }
+}
---
+++ ibmi_projects/ObjectCompiler/src/test/java/com/github/kraudy/compiler/ObjectCompilerTest.java
@@ -0,0 +1,18 @@
+package com.github.kraudy.compiler;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class ObjectCompilerTest {
+
+  /**
+   * Rigorous Test :P
+   */
+  @Test
+  void testApp() {
+      assertTrue(true);
+  }
+}
