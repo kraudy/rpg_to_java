ae5b40ae30e43d93b5615ed88328428640cf0df4
Roberto Torrez <robkraudy@gmail.com>
2025-09-05 12:31:55 -0600

    Refacto enum maps
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- ibmi_projects/ObjectCompiler/src/main/java/com/github/kraudy/compiler/ObjectCompiler.java
+++ ibmi_projects/ObjectCompiler/src/main/java/com/github/kraudy/compiler/ObjectCompiler.java
@@ -28,6 +28,7 @@ import com.ibm.as400.access.User;
 
 import io.github.theprez.dotenv_ibmi.IBMiDotEnv;
 import com.github.kraudy.Nodes;
+import com.github.kraudy.ObjectDependency;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -52,12 +53,11 @@ public class ObjectCompiler implements Runnable{
 
   enum ObjectType { PGM, SRVPGM, MODULE, TABLE, LF, VIEW, ALIAS, PROCEDURE, FUNCTION } // Add more as needed
   
-  enum ComCmd { CRTRPGMOD, CRTSQLRPGI, CRTBNDRPG, CRTRPGPGM, CRTCLMOD, CRTBNDCL, CRTCLPGM, RUNSQLSTM, CRTSRVPGM, CRTDSPF, CRTLF,
+  enum CompCmd { CRTRPGMOD, CRTSQLRPGI, CRTBNDRPG, CRTRPGPGM, CRTCLMOD, CRTBNDCL, CRTCLPGM, RUNSQLSTM, CRTSRVPGM, CRTDSPF, CRTLF,
                CRTPRTF, CRTMNU, CRTQMQRY}
 
-  enum ReqCmd { PGM, OBJ, OBJTYPE, OUTPUT, OUTMBR, MODULE, BNDSRVPGM, LIBL, SRCFILE } 
-
-  enum OptCmd { ACTGRP, DFTACTGRP, BNDDIR, COMMIT, OBJTYPE, TEXT, TGTCCSID, CRTFRMSTMF } 
+  enum ParamCmd { PGM, OBJ, OBJTYPE, OUTPUT, OUTMBR, MODULE, BNDSRVPGM, LIBL, SRCFILE, 
+    ACTGRP, DFTACTGRP, BNDDIR, COMMIT, TEXT, TGTCCSID, CRTFRMSTMF } 
 
   enum ValCmd { FIRST, REPLACE, OUTFILE, LIBL, FILE, DTAARA, PGM, SRVPGM } // add * to these 
 
@@ -72,82 +72,101 @@ public class ObjectCompiler implements Runnable{
   @Option(names = "--source-type", description = "Source type (e.g., RPGLE, CLLE)", converter = SourceTypeConverter.class)
   private SourceType sourceType;
 
-  private static final Map<SourceType, Map<ObjectType, ComCmd>> typeToCmdMap = new EnumMap<>(SourceType.class);  
-  private static final Map<ComCmd, Set<ReqCmd>> requiredParamsMap = new EnumMap<>(ComCmd.class);  
-  private static final Map<ComCmd, Set<OptCmd>> optionalParamsMap = new EnumMap<>(ComCmd.class);
+  /* Maps source type to its compilation command */
+  private static final Map<SourceType, Map<ObjectType, CompCmd>> typeToCmdMap = new EnumMap<>(SourceType.class);  
+
+  /* Maps compilation commands to required and optional params */
+  private static final Map<CompCmd, Set<ParamCmd>> requiredParamsMap = new EnumMap<>(CompCmd.class);  
+  private static final Map<CompCmd, Set<ParamCmd>> optionalParamsMap = new EnumMap<>(CompCmd.class);
+
+  /* Maps params to values */
+  private static final Map<ParamCmd, Set<ValCmd>> valueParamsMap = new EnumMap<>(ParamCmd.class); 
 
   static {
     /*
-     * Populate mapping from (SourceType, ObjectType) to ComCmd
+     * Populate mapping from (SourceType, ObjectType) to CompCmd
      */
     // TODO: There has to be a cleaner way of doing this, maybe using :: or lambda to auto define them
     // Map RPG commands and add bind them to the type
-    Map<ObjectType, ComCmd> rpgMap = new EnumMap<>(ObjectType.class);
-    rpgMap.put(ObjectType.PGM, ComCmd.CRTRPGPGM);
+    Map<ObjectType, CompCmd> rpgMap = new EnumMap<>(ObjectType.class);
+    rpgMap.put(ObjectType.PGM, CompCmd.CRTRPGPGM);
     typeToCmdMap.put(SourceType.RPG, rpgMap);
 
-    Map<ObjectType, ComCmd> rpgLeMap = new EnumMap<>(ObjectType.class);
-    rpgLeMap.put(ObjectType.MODULE, ComCmd.CRTRPGMOD);
-    rpgLeMap.put(ObjectType.PGM, ComCmd.CRTBNDRPG);
-    rpgLeMap.put(ObjectType.SRVPGM, ComCmd.CRTSRVPGM); // Assuming compilation involves module creation first, but mapping to final command
+    Map<ObjectType, CompCmd> rpgLeMap = new EnumMap<>(ObjectType.class);
+    rpgLeMap.put(ObjectType.MODULE, CompCmd.CRTRPGMOD);
+    rpgLeMap.put(ObjectType.PGM, CompCmd.CRTBNDRPG);
+    rpgLeMap.put(ObjectType.SRVPGM, CompCmd.CRTSRVPGM); // Assuming compilation involves module creation first, but mapping to final command
     typeToCmdMap.put(SourceType.RPGLE, rpgLeMap);
 
-    Map<ObjectType, ComCmd> sqlRpgLeMap = new EnumMap<>(ObjectType.class);
-    sqlRpgLeMap.put(ObjectType.MODULE, ComCmd.CRTSQLRPGI);
-    sqlRpgLeMap.put(ObjectType.PGM, ComCmd.CRTSQLRPGI);
-    sqlRpgLeMap.put(ObjectType.SRVPGM, ComCmd.CRTSRVPGM);
+    Map<ObjectType, CompCmd> sqlRpgLeMap = new EnumMap<>(ObjectType.class);
+    sqlRpgLeMap.put(ObjectType.MODULE, CompCmd.CRTSQLRPGI);
+    sqlRpgLeMap.put(ObjectType.PGM, CompCmd.CRTSQLRPGI);
+    sqlRpgLeMap.put(ObjectType.SRVPGM, CompCmd.CRTSRVPGM);
     typeToCmdMap.put(SourceType.SQLRPGLE, sqlRpgLeMap);
 
-    Map<ObjectType, ComCmd> clpMap = new EnumMap<>(ObjectType.class);
-    clpMap.put(ObjectType.PGM, ComCmd.CRTCLPGM);
+    Map<ObjectType, CompCmd> clpMap = new EnumMap<>(ObjectType.class);
+    clpMap.put(ObjectType.PGM, CompCmd.CRTCLPGM);
     typeToCmdMap.put(SourceType.CLP, clpMap);
 
-    Map<ObjectType, ComCmd> clleMap = new EnumMap<>(ObjectType.class);
-    clleMap.put(ObjectType.MODULE, ComCmd.CRTCLMOD);
-    clleMap.put(ObjectType.PGM, ComCmd.CRTBNDCL);
-    clleMap.put(ObjectType.SRVPGM, ComCmd.CRTSRVPGM);
+    Map<ObjectType, CompCmd> clleMap = new EnumMap<>(ObjectType.class);
+    clleMap.put(ObjectType.MODULE, CompCmd.CRTCLMOD);
+    clleMap.put(ObjectType.PGM, CompCmd.CRTBNDCL);
+    clleMap.put(ObjectType.SRVPGM, CompCmd.CRTSRVPGM);
     typeToCmdMap.put(SourceType.CLLE, clleMap);
 
-    Map<ObjectType, ComCmd> sqlMap = new EnumMap<>(ObjectType.class);
-    sqlMap.put(ObjectType.TABLE, ComCmd.RUNSQLSTM);
-    sqlMap.put(ObjectType.LF, ComCmd.RUNSQLSTM);
-    sqlMap.put(ObjectType.VIEW, ComCmd.RUNSQLSTM);
-    sqlMap.put(ObjectType.ALIAS, ComCmd.RUNSQLSTM);
-    sqlMap.put(ObjectType.PROCEDURE, ComCmd.RUNSQLSTM);
-    sqlMap.put(ObjectType.FUNCTION, ComCmd.RUNSQLSTM);
+    Map<ObjectType, CompCmd> sqlMap = new EnumMap<>(ObjectType.class);
+    sqlMap.put(ObjectType.TABLE, CompCmd.RUNSQLSTM);
+    sqlMap.put(ObjectType.LF, CompCmd.RUNSQLSTM);
+    sqlMap.put(ObjectType.VIEW, CompCmd.RUNSQLSTM);
+    sqlMap.put(ObjectType.ALIAS, CompCmd.RUNSQLSTM);
+    sqlMap.put(ObjectType.PROCEDURE, CompCmd.RUNSQLSTM);
+    sqlMap.put(ObjectType.FUNCTION, CompCmd.RUNSQLSTM);
     typeToCmdMap.put(SourceType.SQL, sqlMap);
 
-    // Populate required params for each ComCmd
-    requiredParamsMap.put(ComCmd.CRTRPGMOD, EnumSet.of(ReqCmd.MODULE));
-    requiredParamsMap.put(ComCmd.CRTSQLRPGI, EnumSet.of(ReqCmd.OBJ, ReqCmd.OBJTYPE));
-    requiredParamsMap.put(ComCmd.CRTBNDRPG, EnumSet.of(ReqCmd.PGM));
-    requiredParamsMap.put(ComCmd.CRTRPGPGM, EnumSet.of(ReqCmd.PGM));
-    requiredParamsMap.put(ComCmd.CRTCLMOD, EnumSet.of(ReqCmd.MODULE));
-    requiredParamsMap.put(ComCmd.CRTBNDCL, EnumSet.of(ReqCmd.PGM));
-    requiredParamsMap.put(ComCmd.CRTCLPGM, EnumSet.of(ReqCmd.PGM));
-    requiredParamsMap.put(ComCmd.RUNSQLSTM, EnumSet.noneOf(ReqCmd.SRCFILE));
-    requiredParamsMap.put(ComCmd.CRTSRVPGM, EnumSet.of(ReqCmd.OBJ, ReqCmd.MODULE, ReqCmd.BNDSRVPGM));
-    requiredParamsMap.put(ComCmd.CRTDSPF, EnumSet.of(ReqCmd.OBJ));
-    requiredParamsMap.put(ComCmd.CRTLF, EnumSet.of(ReqCmd.OBJ));
-    requiredParamsMap.put(ComCmd.CRTPRTF, EnumSet.of(ReqCmd.OBJ));
-    requiredParamsMap.put(ComCmd.CRTMNU, EnumSet.of(ReqCmd.OBJ));
-    requiredParamsMap.put(ComCmd.CRTQMQRY, EnumSet.of(ReqCmd.OBJ));
+    // Populate required params for each CompCmd
+    requiredParamsMap.put(CompCmd.CRTRPGMOD, EnumSet.of(ParamCmd.MODULE));
+    requiredParamsMap.put(CompCmd.CRTSQLRPGI, EnumSet.of(ParamCmd.OBJ, ParamCmd.OBJTYPE));
+    requiredParamsMap.put(CompCmd.CRTBNDRPG, EnumSet.of(ParamCmd.PGM));
+    requiredParamsMap.put(CompCmd.CRTRPGPGM, EnumSet.of(ParamCmd.PGM));
+    requiredParamsMap.put(CompCmd.CRTCLMOD, EnumSet.of(ParamCmd.MODULE));
+    requiredParamsMap.put(CompCmd.CRTBNDCL, EnumSet.of(ParamCmd.PGM));
+    requiredParamsMap.put(CompCmd.CRTCLPGM, EnumSet.of(ParamCmd.PGM));
+    requiredParamsMap.put(CompCmd.RUNSQLSTM, EnumSet.of(ParamCmd.SRCFILE));
+    requiredParamsMap.put(CompCmd.CRTSRVPGM, EnumSet.of(ParamCmd.OBJ, ParamCmd.MODULE, ParamCmd.BNDSRVPGM));
+    requiredParamsMap.put(CompCmd.CRTDSPF, EnumSet.of(ParamCmd.OBJ));
+    requiredParamsMap.put(CompCmd.CRTLF, EnumSet.of(ParamCmd.OBJ));
+    requiredParamsMap.put(CompCmd.CRTPRTF, EnumSet.of(ParamCmd.OBJ));
+    requiredParamsMap.put(CompCmd.CRTMNU, EnumSet.of(ParamCmd.OBJ));
+    requiredParamsMap.put(CompCmd.CRTQMQRY, EnumSet.of(ParamCmd.OBJ));
 
-    // Populate optional params for each ComCmd
-    optionalParamsMap.put(ComCmd.CRTRPGMOD, EnumSet.of(OptCmd.TEXT, OptCmd.TGTCCSID, OptCmd.BNDDIR, OptCmd.DFTACTGRP));
-    optionalParamsMap.put(ComCmd.CRTSQLRPGI, EnumSet.of(OptCmd.COMMIT, OptCmd.OBJTYPE, OptCmd.TEXT, OptCmd.ACTGRP, OptCmd.BNDDIR));
-    optionalParamsMap.put(ComCmd.CRTBNDRPG, EnumSet.of(OptCmd.ACTGRP, OptCmd.DFTACTGRP, OptCmd.BNDDIR, OptCmd.TEXT));
-    optionalParamsMap.put(ComCmd.CRTRPGPGM, EnumSet.of(OptCmd.TEXT, OptCmd.TGTCCSID));
-    optionalParamsMap.put(ComCmd.CRTCLMOD, EnumSet.of(OptCmd.TEXT, OptCmd.TGTCCSID, OptCmd.ACTGRP));
-    optionalParamsMap.put(ComCmd.CRTBNDCL, EnumSet.of(OptCmd.ACTGRP, OptCmd.DFTACTGRP, OptCmd.BNDDIR, OptCmd.TEXT));
-    optionalParamsMap.put(ComCmd.CRTCLPGM, EnumSet.of(OptCmd.TEXT, OptCmd.TGTCCSID));
-    optionalParamsMap.put(ComCmd.RUNSQLSTM, EnumSet.of(OptCmd.COMMIT, OptCmd.TEXT));
-    optionalParamsMap.put(ComCmd.CRTSRVPGM, EnumSet.of(OptCmd.ACTGRP, OptCmd.BNDDIR, OptCmd.TEXT, OptCmd.DFTACTGRP));
-    optionalParamsMap.put(ComCmd.CRTDSPF, EnumSet.of(OptCmd.TEXT, OptCmd.TGTCCSID));
-    optionalParamsMap.put(ComCmd.CRTLF, EnumSet.of(OptCmd.TEXT, OptCmd.TGTCCSID));
-    optionalParamsMap.put(ComCmd.CRTPRTF, EnumSet.of(OptCmd.TEXT, OptCmd.TGTCCSID));
-    optionalParamsMap.put(ComCmd.CRTMNU, EnumSet.of(OptCmd.TEXT));
-    optionalParamsMap.put(ComCmd.CRTQMQRY, EnumSet.of(OptCmd.TEXT));  
+    // Populate optional params for each CompCmd
+    optionalParamsMap.put(CompCmd.CRTRPGMOD, EnumSet.of(ParamCmd.TEXT, ParamCmd.TGTCCSID, ParamCmd.BNDDIR, ParamCmd.DFTACTGRP));
+    optionalParamsMap.put(CompCmd.CRTSQLRPGI, EnumSet.of(ParamCmd.COMMIT, ParamCmd.TEXT, ParamCmd.ACTGRP, ParamCmd.BNDDIR));
+    optionalParamsMap.put(CompCmd.CRTBNDRPG, EnumSet.of(ParamCmd.ACTGRP, ParamCmd.DFTACTGRP, ParamCmd.BNDDIR, ParamCmd.TEXT));
+    optionalParamsMap.put(CompCmd.CRTRPGPGM, EnumSet.of(ParamCmd.TEXT, ParamCmd.TGTCCSID));
+    optionalParamsMap.put(CompCmd.CRTCLMOD, EnumSet.of(ParamCmd.TEXT, ParamCmd.TGTCCSID, ParamCmd.ACTGRP));
+    optionalParamsMap.put(CompCmd.CRTBNDCL, EnumSet.of(ParamCmd.ACTGRP, ParamCmd.DFTACTGRP, ParamCmd.BNDDIR, ParamCmd.TEXT));
+    optionalParamsMap.put(CompCmd.CRTCLPGM, EnumSet.of(ParamCmd.TEXT, ParamCmd.TGTCCSID));
+    optionalParamsMap.put(CompCmd.RUNSQLSTM, EnumSet.of(ParamCmd.COMMIT, ParamCmd.TEXT));
+    optionalParamsMap.put(CompCmd.CRTSRVPGM, EnumSet.of(ParamCmd.ACTGRP, ParamCmd.BNDDIR, ParamCmd.TEXT, ParamCmd.DFTACTGRP));
+    optionalParamsMap.put(CompCmd.CRTDSPF, EnumSet.of(ParamCmd.TEXT, ParamCmd.TGTCCSID));
+    optionalParamsMap.put(CompCmd.CRTLF, EnumSet.of(ParamCmd.TEXT, ParamCmd.TGTCCSID));
+    optionalParamsMap.put(CompCmd.CRTPRTF, EnumSet.of(ParamCmd.TEXT, ParamCmd.TGTCCSID));
+    optionalParamsMap.put(CompCmd.CRTMNU, EnumSet.of(ParamCmd.TEXT));
+    optionalParamsMap.put(CompCmd.CRTQMQRY, EnumSet.of(ParamCmd.TEXT));
+
+    // Populate valueParamsMap with special values for each parameter (add * when using in commands)
+    valueParamsMap.put(ParamCmd.OUTPUT, EnumSet.of(ValCmd.OUTFILE));
+    valueParamsMap.put(ParamCmd.OUTMBR, EnumSet.of(ValCmd.FIRST, ValCmd.REPLACE));
+    valueParamsMap.put(ParamCmd.OBJTYPE, EnumSet.of(ValCmd.PGM, ValCmd.SRVPGM));
+    valueParamsMap.put(ParamCmd.MODULE, EnumSet.of(ValCmd.PGM));
+    valueParamsMap.put(ParamCmd.BNDSRVPGM, EnumSet.of(ValCmd.SRVPGM));
+    valueParamsMap.put(ParamCmd.LIBL, EnumSet.of(ValCmd.LIBL));
+    valueParamsMap.put(ParamCmd.SRCFILE, EnumSet.of(ValCmd.FILE, ValCmd.LIBL));
+    valueParamsMap.put(ParamCmd.PGM, EnumSet.of(ValCmd.LIBL, ValCmd.PGM));
+    valueParamsMap.put(ParamCmd.OBJ, EnumSet.of(ValCmd.LIBL, ValCmd.FILE, ValCmd.DTAARA));
+    // TODO: for parms with no defined value: EnumSet.noneOf(ValCmd.class)
+
   }
 
   static class ObjectNameConverter implements ITypeConverter<String> {
@@ -186,24 +205,74 @@ public class ObjectCompiler implements Runnable{
   @Override
   public void run() {
     // For now, demonstrate the mappings
-    Map<ObjectType, ComCmd> objectMap = typeToCmdMap.get(sourceType);
+    Map<ObjectType, CompCmd> objectMap = typeToCmdMap.get(sourceType);
     if (objectMap == null) {
       System.err.println("No mapping for source type: " + sourceType);
       return;
     }
-    ComCmd comCmd = objectMap.get(objectType);
-    if (comCmd == null) {
+    CompCmd CompCmd = objectMap.get(objectType);
+    if (CompCmd == null) {
       System.err.println("No compilation command for source type " + sourceType + " and object type " + objectType);
       return;
     }
-    Set<ReqCmd> reqParams = requiredParamsMap.getOrDefault(comCmd, EnumSet.noneOf(ReqCmd.class));
-    Set<OptCmd> optParams = optionalParamsMap.getOrDefault(comCmd, EnumSet.noneOf(OptCmd.class));System.out.println("Compilation command: " + comCmd.name());
+    Set<ParamCmd> reqParams = requiredParamsMap.getOrDefault(CompCmd, EnumSet.noneOf(ParamCmd.class));
+    Set<ParamCmd> optParams = optionalParamsMap.getOrDefault(CompCmd, EnumSet.noneOf(ParamCmd.class));
+
+    System.out.println("Compilation command: " + CompCmd.name());
     System.out.println("Required parameters: " + reqParams.stream().map(Enum::name).collect(Collectors.joining(", ")));
     System.out.println("Optional parameters: " + optParams.stream().map(Enum::name).collect(Collectors.joining(", ")));
     // Later: build command string, execute via CALL QCMD, etc.  
   }
 
+  public ObjectCompiler(AS400 system) throws Exception {
+    this(system, new AS400JDBCDataSource(system).getConnection());
+  }
+
+  public ObjectCompiler(AS400 system, Connection connection) throws Exception {
+    this.system = system;
+
+    // Database
+    this.connection = connection;
+    this.connection.setAutoCommit(true);
+
+    // User
+    this.currentUser = new User(system, system.getUserId());
+    this.currentUser.loadUserInformation();
+  }
+
+  private void compile(){
+    try {
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally{
+      cleanup();
+    }
+  }
+
+  private void cleanup(){
+    try {
+      if (connection != null && !connection.isClosed()) {
+        connection.close();
+      }
+      if (system != null) {
+        system.disconnectAllServices();
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
   static void main( String... args ){
-    CommandLine(new ObjectCompiler()).execute(args);
+    AS400 system = null;
+    ObjectCompiler compiler = null;
+    try {
+      system = IBMiDotEnv.getNewSystemConnection(true); // Get system
+      compiler = new ObjectCompiler(system);
+      new CommandLine(compiler).execute(args);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
