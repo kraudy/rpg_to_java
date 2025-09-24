cf89421b9b99074256a3c03ace7a92bd4de30404
Roberto Torrez <robkraudy@gmail.com>
2025-09-24 07:43:13 -0600

    Create proj api caller
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

---
+++ ibmi_projects/ApiCaller/src/main/java/com/github/kraudy/api/ApiCaller.java
@@ -0,0 +1,11 @@
+package com.github.kraudy.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class ApiCaller {
+  public static void main( String[] args ){
+    System.out.println( "Hello World!" );
+  }
+}
---
+++ ibmi_projects/ApiCaller/src/test/java/com/github/kraudy/api/ApiCaller.java
@@ -0,0 +1,18 @@
+package com.github.kraudy.api;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApiCaller {
+
+  /**
+   * Rigorous Test :P
+   */
+  @Test
+  void testApp() {
+      assertTrue(true);
+  }
+}
