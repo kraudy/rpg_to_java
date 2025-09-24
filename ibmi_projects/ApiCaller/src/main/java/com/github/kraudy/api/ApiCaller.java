36cefc09d26465a1a45588c8f8e3126aa5a40f52
Roberto Torrez <robkraudy@gmail.com>
2025-09-24 07:56:50 -0600

    Add some code
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- ibmi_projects/ApiCaller/src/main/java/com/github/kraudy/api/ApiCaller.java
+++ ibmi_projects/ApiCaller/src/main/java/com/github/kraudy/api/ApiCaller.java
@@ -1,11 +1,198 @@
 package com.github.kraudy.api;
 
-/**
- * Hello world!
- *
- */
+import java.beans.PropertyVetoException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ibm.as400.access.AS400;
+import com.ibm.as400.access.AS400Message;
+import com.ibm.as400.access.AS400SecurityException;
+import com.ibm.as400.access.ErrorCompletingRequestException;
+import com.ibm.as400.access.AS400JDBCDataSource;
+import com.ibm.as400.access.CommandCall;
+import com.ibm.as400.access.User;
+
+import io.github.theprez.dotenv_ibmi.IBMiDotEnv;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command (name = "apiCaller", description = "System API Caller", mixinStandardHelpOptions = true, version = "ApiCaller 0.0.1")
 public class ApiCaller {
-  public static void main( String[] args ){
-    System.out.println( "Hello World!" );
+  public static final String INVARIANT_CCSID = "37"; // EBCDIC
+  private final AS400 system;
+  private final Connection connection;
+  private final User currentUser;
+
+  public enum SourceType { 
+    RPG, RPGLE, SQLRPGLE, CLP, CLLE, SQL;
+
+    public static SourceType fromString(String value) {
+      try {
+        return SourceType.valueOf(value);
+      } catch (IllegalArgumentException e) {
+        throw new IllegalArgumentException("Could not get source type from object attribute '" + value + "'");
+      }
+    } 
+  }
+
+  public enum ObjectType { PGM, SRVPGM, MODULE, TABLE, LF, VIEW, ALIAS, PROCEDURE, FUNCTION;
+    public String toParam(){
+      return "*" + this.name();
+    }
+  }
+
+  public enum ValCmd { 
+    FIRST, REPLACE, OUTFILE, LIBL, FILE, DTAARA, PGM, MODULE, OBJ, SRVPGM, CURLIB, ALL, CURRENT,
+    NONE, BASIC, FULL,
+
+    YES, NO, STMT, SOURCE, LIST, HEX, JOBRUN, USER, LIBCRTAUT, PEP, NOCOL, PRINT, SNGLVL; 
+
+    public static ValCmd fromString(String value) {
+      try {
+          return ValCmd.valueOf(value.substring(1)); // Remove the leading "*" and convert to enum
+      } catch (IllegalArgumentException e) {
+          throw new IllegalArgumentException("Could not convert '" + value + "' to ValCmd. Unknown value: '" + value + "'");
+      }
+    }
+
+    @Override
+    public String toString() {
+        return "*" + name();
+    }  
+  }
+
+  static class LibraryConverter implements CommandLine.ITypeConverter<String> {
+    @Override
+    public String convert(String value) throws Exception {
+      try{
+        value = value.trim().toUpperCase();
+        if (value.length() > 10 || value.isEmpty()) {
+          throw new Exception("Invalid library name: must be 1-10 characters");
+        }
+        return value;
+
+      } catch (IllegalArgumentException e) {
+        throw new Exception("Invalid library name: " + value);
+      }
+    }
+  }  
+
+  static class ObjectNameConverter implements CommandLine.ITypeConverter<String> {
+    @Override
+    public String convert(String value) throws Exception {
+      try{
+        value = value.trim().toUpperCase();
+        if (value.length() > 10 || value.isEmpty()) {
+          throw new Exception("Invalid object name: must be 1-10 characters");
+        }
+        return value;
+
+      } catch (IllegalArgumentException e) {
+        throw new Exception("Invalid object name: " + value);
+      }
+    }
+  }  
+
+  static class ObjectTypeConverter implements CommandLine.ITypeConverter<ObjectType> {
+    @Override
+    public ObjectType convert(String value) throws Exception {
+      try {
+        return ObjectType.valueOf(value.trim().toUpperCase());
+      } catch (IllegalArgumentException e) {
+        throw new Exception("Invalid object type: " + value);
+      }
+    }
+  }  
+
+  static class SourceTypeConverter implements CommandLine.ITypeConverter<SourceType> {
+    @Override
+    public SourceType convert(String value) throws Exception {
+      try {
+        return SourceType.valueOf(value.trim().toUpperCase());
+      } catch (IllegalArgumentException e) {
+        throw new Exception("Invalid source type: " + value);
+      }
+    }
+  }
+
+  /* Object attributes. Required params */
+  @Option(names = { "-l", "--lib" }, required = true, description = "Target library for object", converter = LibraryConverter.class)
+  private String library;
+
+  //TODO: --obj, -t and -st could be made in one: hello.pgm.rpgle and just parse it. Maybe the last could be optional if the object is found.
+  @Option(names = "--obj", required = true, description = "Object name", converter = ObjectNameConverter.class)
+  private String objectName;
+
+  @Option(names = {"-t","--type"}, required = true, description = "Object type (e.g., PGM, SRVPGM)", converter = ObjectTypeConverter.class)
+  private ObjectType objectType;
+
+  /* Source-related params. Good to have */
+  @Option(names = { "-sl", "--source-lib" }, description = "Source library (defaults to *LIBL or retrieved from object)", converter = LibraryConverter.class)
+  private String sourceLib = ValCmd.LIBL.toString(); //"*LIBL"
+
+  @Option(names = { "-sf", "--source-file" }, description = "Source physical file (defaults based on source type or retrieved from object)")
+  private String sourceFile = "";
+
+  @Option(names = { "-sn", "--source-name" }, description = "Source member name (defaults to object name, command-specific *SPECIAL, or retrieved from object)")
+  private String sourceName = "";
+
+  @Option(names = {"-st","--source-type"}, description = "Source type (e.g., RPGLE, CLLE) (defaults to retrieved from object if possible)", converter = SourceTypeConverter.class)
+  private SourceType sourceType;
+
+  /* Constructors */
+  public ApiCaller(AS400 system) throws Exception {
+    this(system, new AS400JDBCDataSource(system).getConnection());
+  }
+
+  public ApiCaller(AS400 system, Connection connection) throws Exception {
+    this.system = system;
+
+    // Database
+    this.connection = connection;
+    this.connection.setAutoCommit(true);
+
+    // User
+    this.currentUser = new User(system, system.getUserId());
+    this.currentUser.loadUserInformation();
+
+  }
+
+  public void run(){
+
+  }
+
+  private void cleanup(){
+    try {
+      if (connection != null && !connection.isClosed()) {
+        connection.close();
+      }
+      if (system != null) {
+        system.disconnectAllServices();
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main( String... args ){
+    AS400 system = null;
+    ApiCaller apiCaller = null;
+    try {
+      system = IBMiDotEnv.getNewSystemConnection(true); // Get system
+      apiCaller = new ApiCaller(system);
+      new CommandLine(apiCaller).execute(args);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
