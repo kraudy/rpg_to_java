a3ec3ffbf662992ceb353ed38c39f23d52dfb0fa
Roberto Torrez <robkraudy@gmail.com>
2025-10-19 06:48:18 -0600

    Add ParamMap class
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- ibmi_projects/ObjectCompiler/src/main/java/com/github/kraudy/compiler/ParamMap.java
+++ ibmi_projects/ObjectCompiler/src/main/java/com/github/kraudy/compiler/ParamMap.java
@@ -6,5 +6,47 @@ import com.github.kraudy.compiler.CompilationPattern.ParamCmd;
 import com.github.kraudy.compiler.CompilationPattern.ValCmd;
 
 public class ParamMap extends HashMap<ParamCmd, String> {
-    
+    private final boolean debug;
+    //private final boolean verbose;
+
+    public ParamMap(boolean debug) {
+        this.debug = debug;
+    }
+
+    @Override
+    public String put(ParamCmd param, String value) {
+
+        // Validation: Ensure value is a valid ValCmd option if it matches a known pattern
+        //TODO: Add validation here for params with defined values
+        /* 
+        if (value.startsWith("*")) {
+            ValCmd valCmd = ValCmd.valueOf(value.substring(1).toUpperCase());
+            value = ParamCmd.paramValue(param, valCmd);  // Re-validate and normalize
+        }
+        */
+
+        switch (param) {
+          case TEXT:
+            value = "'" + value + "'";
+            break;
+        
+          default:
+            break;
+        }
+
+        String oldValue = super.put(param, value);
+
+        if (debug) {
+          if (oldValue != null) {
+            System.out.println("Previous value. " + param.name() + ": '" + oldValue + "'");
+          } 
+          System.out.println("Current value. " + param.name() + ": '" + value + "'");
+        }
+
+        return oldValue;
+    }
+
+    public String put(ParamCmd param, ValCmd value) {
+        return put(param, value.toString());
+    }
 }
