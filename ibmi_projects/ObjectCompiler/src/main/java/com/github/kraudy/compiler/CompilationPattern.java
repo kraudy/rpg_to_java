2ef94dd01711ac4995ec22fa3bb095c8cb4c655c
Roberto Torrez <robkraudy@gmail.com>
2025-11-19 20:25:02 -0600

    Big refacto
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- ibmi_projects/ObjectCompiler/src/main/java/com/github/kraudy/compiler/CompilationPattern.java
+++ ibmi_projects/ObjectCompiler/src/main/java/com/github/kraudy/compiler/CompilationPattern.java
@@ -1,10 +1,13 @@
 package com.github.kraudy.compiler;
 
+import java.util.Arrays;
 import java.util.EnumMap;
+import java.util.List;
 import java.util.Map;
 
 import com.github.kraudy.compiler.ObjectDescription.ObjectType;
 import com.github.kraudy.compiler.ObjectDescription.SourceType;
+import com.github.kraudy.compiler.ObjectDescription.SysCmd;
 import com.github.kraudy.migrator.SourceMigrator;
 
 
@@ -145,6 +148,590 @@ public class CompilationPattern {
     ddsMap.put(ObjectType.LF, CompCmd.CRTLF);
     typeToCmdMap.put(SourceType.DDS, ddsMap);
   }  
+
+  /* System command patterns */
+  public static final List<ParamCmd> ChgLibLPattern = Arrays.asList(
+    ParamCmd.LIBL,
+    ParamCmd.CURLIB // Add USRLIBL if needed and added to ParamCmd
+  );
+
+  public static final List<ParamCmd> ChgCurLibPattern = Arrays.asList(
+    ParamCmd.CURLIB
+  );
+
+  
+  /* Maps compilation command to its pattern */
+  public static final Map<SysCmd, List<ParamCmd>> SysCmdToPatternMap = new EnumMap<>(SysCmd.class);
+  static{
+    /* Libraries */
+    SysCmdToPatternMap.put(SysCmd.CHGLIBL, ChgLibLPattern);
+    SysCmdToPatternMap.put(SysCmd.CHGCURLIB, ChgCurLibPattern);
+  }
+
+  /* ILE Patterns */
+
+  // CRTSRVPGM
+  public static final List<ParamCmd> SrvpgmPattern = Arrays.asList(
+    ParamCmd.SRVPGM,
+    ParamCmd.MODULE,
+    ParamCmd.EXPORT,  
+    ParamCmd.SRCFILE, 
+    ParamCmd.SRCMBR,  
+    ParamCmd.SRCSTMF, 
+    ParamCmd.TEXT,    
+    ParamCmd.BNDSRVPGM,
+    ParamCmd.BNDDIR,
+    ParamCmd.ACTGRP,
+    ParamCmd.OPTION,
+    ParamCmd.DETAIL,
+    ParamCmd.ALWUPD,    
+    ParamCmd.ALWLIBUPD, 
+    ParamCmd.USRPRF,    
+    ParamCmd.REPLACE,   
+    ParamCmd.AUT,       
+    ParamCmd.TGTRLS,    
+    ParamCmd.ALWRINZ,   
+    ParamCmd.STGMDL,    
+    ParamCmd.ARGOPT,    
+    ParamCmd.IPA,       
+    ParamCmd.IPACTLFILE
+
+  );
+
+  // CRTBNDRPG
+  public static final List<ParamCmd> ileRpgPgmPattern = Arrays.asList(
+    ParamCmd.PGM,       // Program
+    ParamCmd.SRCFILE,   // Source file
+    ParamCmd.SRCMBR,    // Source member
+    ParamCmd.SRCSTMF,   // Source stream file
+
+    ParamCmd.GENLVL,    // Generation severity level
+    ParamCmd.TEXT,      // Text 'description'
+
+    ParamCmd.DFTACTGRP,
+    ParamCmd.ACTGRP,
+    ParamCmd.STGMDL,    //TODO: Maybe this needs to be conditional
+
+    ParamCmd.BNDDIR,    // Binding directory
+
+    ParamCmd.OPTION,    // Compiler options
+
+    ParamCmd.DBGVIEW,   // Debugging views
+    ParamCmd.DBGENCKEY, // Debug encryption key
+    ParamCmd.OUTPUT,    // Output
+    ParamCmd.OPTIMIZE,  // Optimization level
+    ParamCmd.INDENT,    // Source listing indentation
+    ParamCmd.CVTOPT,    // Type conversion options
+
+    ParamCmd.SRTSEQ,    // Sort sequence
+    ParamCmd.LANGID,    // Sort sequence
+    ParamCmd.REPLACE,
+    ParamCmd.USRPRF,
+    ParamCmd.AUT,
+    ParamCmd.TRUNCNBR,
+    ParamCmd.FIXNBR,
+    ParamCmd.TGTRLS,
+    ParamCmd.ALWNULL,
+    ParamCmd.DEFINE,
+    ParamCmd.ENBPFRCOL,
+
+    ParamCmd.PRFDTA,
+    ParamCmd.LICOPT,
+    ParamCmd.INCDIR,
+    ParamCmd.PGMINFO,
+
+    ParamCmd.INFOSTMF,
+    ParamCmd.PPGENOPT,
+    ParamCmd.PPSRCFILE,
+    ParamCmd.PPSRCMBR,
+    ParamCmd.PPSRCSTMF,
+    ParamCmd.TGTCCSID,
+    ParamCmd.REQPREXP,
+    ParamCmd.PPMINOUTLN
+
+  );
+
+  // CRTBNDCL
+  public static final List<ParamCmd> ileClPgmPattern = Arrays.asList(
+    ParamCmd.PGM,       // Program
+    ParamCmd.SRCFILE,   // Source file
+    ParamCmd.SRCMBR,    // Source member
+    ParamCmd.SRCSTMF,   // Source stream file
+    ParamCmd.TEXT,      // Text 'description'
+
+    ParamCmd.DFTACTGRP,
+    ParamCmd.ACTGRP,
+    ParamCmd.STGMDL,
+    ParamCmd.OUTPUT,
+
+    ParamCmd.OPTION,
+    ParamCmd.USRPRF,   
+    ParamCmd.LOG,      
+    ParamCmd.ALWRTVSRC,
+    ParamCmd.REPLACE,  
+    ParamCmd.TGTRLS,   
+    ParamCmd.AUT,      
+    ParamCmd.SRTSEQ,           
+    ParamCmd.LANGID,   
+    ParamCmd.OPTIMIZE, 
+    ParamCmd.DBGVIEW,  
+    ParamCmd.DBGENCKEY,
+    ParamCmd.ENBPFRCOL,
+
+    ParamCmd.INCFILE,
+    ParamCmd.INCDIR,
+    ParamCmd.TGTCCSID
+
+  );
+
+  // Modules 
+
+  // CRTRPGMOD
+  public static final List<ParamCmd> RpgModulePattern = Arrays.asList(
+    ParamCmd.MODULE, 
+    ParamCmd.SRCFILE,
+    ParamCmd.SRCMBR, 
+    ParamCmd.SRCSTMF,
+    ParamCmd.GENLVL, 
+    ParamCmd.TEXT,   
+    ParamCmd.OPTION,   
+    ParamCmd.DBGVIEW,
+    
+    ParamCmd.DBGENCKEY,  
+    ParamCmd.OUTPUT,     
+    ParamCmd.OPTIMIZE,   
+    ParamCmd.INDENT,     
+    ParamCmd.CVTOPT,     
+    ParamCmd.SRTSEQ,     
+    ParamCmd.LANGID,     
+    ParamCmd.REPLACE,    
+    ParamCmd.AUT,        
+    ParamCmd.TRUNCNBR,   
+    ParamCmd.FIXNBR,     
+    ParamCmd.TGTRLS,     
+    ParamCmd.ALWNULL,    
+
+    ParamCmd.DEFINE,            
+    ParamCmd.ENBPFRCOL,
+    ParamCmd.PRFDTA,   
+    ParamCmd.STGMDL,   
+    ParamCmd.BNDDIR,   
+
+    ParamCmd.LICOPT,   
+    ParamCmd.INCDIR,   
+    ParamCmd.PGMINFO,
+    
+    ParamCmd.INFOSTMF,   
+    ParamCmd.PPGENOPT,   
+    ParamCmd.PPSRCFILE,  
+    ParamCmd.PPSRCMBR,   
+    ParamCmd.PPSRCSTMF,  
+    ParamCmd.TGTCCSID,   
+    ParamCmd.REQPREXP,   
+    ParamCmd.PPMINOUTLN
+  );
+
+  // CRTCLMOD
+  public static final List<ParamCmd> ClleModulePattern = Arrays.asList(
+    ParamCmd.MODULE, 
+    ParamCmd.SRCFILE,
+    ParamCmd.SRCMBR, 
+    ParamCmd.SRCSTMF,
+    ParamCmd.TEXT,   
+    ParamCmd.OUTPUT, 
+    ParamCmd.OPTION,   
+
+    ParamCmd.LOG,
+    ParamCmd.ALWRTVSRC, 
+    ParamCmd.REPLACE,   
+    ParamCmd.TGTRLS,    
+    ParamCmd.AUT,       
+    ParamCmd.SRTSEQ,    
+    ParamCmd.LANGID,    
+    ParamCmd.OPTIMIZE,  
+    ParamCmd.DBGVIEW,   
+    ParamCmd.DBGENCKEY, 
+    ParamCmd.ENBPFRCOL, 
+    ParamCmd.INCFILE,  
+    ParamCmd.INCDIR,
+    ParamCmd.TGTCCSID
+
+  );
+
+  // Sql and RPG
+
+  // CRTSQLRPGI
+  public static final List<ParamCmd> SqlRpgPgmPattern = Arrays.asList(
+    ParamCmd.OBJ, 
+    ParamCmd.SRCFILE,
+    ParamCmd.SRCMBR, 
+    ParamCmd.SRCSTMF,
+
+    ParamCmd.COMMIT,  
+    ParamCmd.RDB,     
+    ParamCmd.OBJTYPE, 
+    ParamCmd.OUTPUT,  
+    ParamCmd.TEXT,   
+    ParamCmd.OPTION,    
+    ParamCmd.RPGPPOPT,  
+    ParamCmd.TGTRLS,    
+    ParamCmd.INCFILE,   
+    ParamCmd.INCDIR,    
+    ParamCmd.ALWCPYDTA, 
+    ParamCmd.CLOSQLCSR, 
+    ParamCmd.ALWBLK,    
+    ParamCmd.DLYPRP,    
+    ParamCmd.CONACC,       
+    ParamCmd.GENLVL,    
+    ParamCmd.DATFMT,    
+    ParamCmd.DATSEP,    
+    ParamCmd.TIMFMT,    
+    ParamCmd.TIMSEP,    
+    ParamCmd.REPLACE,   
+    ParamCmd.RDBCNNMTH, 
+    ParamCmd.DFTRDBCOL, 
+    ParamCmd.DYNDFTCOL, 
+    ParamCmd.SQLPKG, 
+    ParamCmd.SQLPATH,
+    ParamCmd.SQLCURR,
+    ParamCmd.SAAFLAG,
+    ParamCmd.FLAGSTD,
+    ParamCmd.PRTFILE,  
+    ParamCmd.DBGVIEW,  
+    ParamCmd.DBGENCKEY,
+    ParamCmd.USRPRF,   
+    ParamCmd.DYNUSRPRF,
+    ParamCmd.SRTSEQ,   
+    ParamCmd.LANGID,   
+    ParamCmd.CVTCCSID, 
+    ParamCmd.TOSRCFILE,
+    ParamCmd.DECRESULT,
+    ParamCmd.DECFLTRND, 
+    ParamCmd.COMPILEOPT
+
+  );
+
+  /* OPM */
+
+  // CRTRPGPGM
+  public static final List<ParamCmd> opmRpgPgmPattern = Arrays.asList(
+    ParamCmd.PGM,       // Program
+    ParamCmd.SRCFILE,   // Source file
+    ParamCmd.SRCMBR,    // Source member
+    ParamCmd.GENLVL,   
+    ParamCmd.TEXT,   
+
+    ParamCmd.OPTION,   
+    ParamCmd.GENOPT,   
+    ParamCmd.INDENT,   
+
+    ParamCmd.CVTOPT,  
+    ParamCmd.SRTSEQ,  
+    ParamCmd.LANGID,  
+    ParamCmd.SAAFLAG, 
+    ParamCmd.PRTFILE, 
+    ParamCmd.REPLACE, 
+    ParamCmd.TGTRLS,  
+    ParamCmd.USRPRF,  
+    ParamCmd.AUT,     
+    ParamCmd.PHSTRC,  
+    ParamCmd.ITDUMP,  
+
+    ParamCmd.SNPDUMP,   
+    ParamCmd.CODELIST,  
+    ParamCmd.IGNDECERR, 
+    ParamCmd.ALWNULL  
+
+  );
+
+  // CRTCLPGM
+  public static final List<ParamCmd> opmClPgmPattern = Arrays.asList(
+    ParamCmd.PGM,       // Program
+    ParamCmd.SRCFILE,   // Source file
+    ParamCmd.SRCMBR,    // Source member
+    ParamCmd.TEXT,   
+
+    ParamCmd.OUTPUT,
+    ParamCmd.OPTION,
+    ParamCmd.GENOPT,
+    ParamCmd.USRPRF,
+
+    ParamCmd.LOG,      
+    ParamCmd.ALWRTVSRC,
+    ParamCmd.REPLACE,  
+    ParamCmd.TGTRLS,   
+    ParamCmd.AUT,      
+    ParamCmd.SRTSEQ,   
+    ParamCmd.LANGID,   
+    ParamCmd.INCFILE  
+
+  );
+
+
+  /* Sql */
+  // RUNSQLSTM
+  public static final List<ParamCmd> SqlPattern = Arrays.asList(
+    ParamCmd.SRCFILE,
+    ParamCmd.SRCMBR, 
+    ParamCmd.SRCSTMF,
+    ParamCmd.COMMIT, 
+    ParamCmd.NAMING,      
+    ParamCmd.ERRLVL, 
+    ParamCmd.DATFMT, 
+    ParamCmd.DATSEP, 
+    ParamCmd.TIMFMT, 
+    ParamCmd.TIMSEP,
+    ParamCmd.MARGINS,
+    ParamCmd.DFTRDBCOL,
+    ParamCmd.SAAFLAG,  
+    ParamCmd.FLAGSTD,  
+    ParamCmd.DECMPT,   
+    ParamCmd.SRTSEQ,   
+    ParamCmd.LANGID,   
+    ParamCmd.OPTION,   
+    ParamCmd.PRTFILE,  
+    ParamCmd.PROCESS,  
+    ParamCmd.SECLVLTXT,
+    ParamCmd.ALWCPYDTA,
+    ParamCmd.ALWBLK,    
+    ParamCmd.SQLCURRULE,
+    ParamCmd.DECRESULT, 
+    ParamCmd.CONACC,    
+    ParamCmd.SYSTIME,   
+    ParamCmd.OUTPUT,    
+    ParamCmd.TGTRLS,    
+    ParamCmd.DBGVIEW,   
+    ParamCmd.CLOSQLCSR, 
+    ParamCmd.DLYPRP,    
+    ParamCmd.USRPRF,    
+    ParamCmd.DYNUSRPRF
+
+  );
+
+  /* DDS Files */
+
+  // CRTDSPF
+  public static final List<ParamCmd> ddsDspfPattern = Arrays.asList(
+    ParamCmd.FILE,    
+    ParamCmd.SRCFILE, 
+    ParamCmd.SRCMBR,  
+    ParamCmd.GENLVL,  
+    ParamCmd.FLAG,    
+    ParamCmd.DEV,     
+    ParamCmd.TEXT,
+    ParamCmd.OPTION,
+    ParamCmd.MAXDEV, 
+    ParamCmd.ENHDSP, 
+    ParamCmd.RSTDSP, 
+    ParamCmd.DFRWRT, 
+    ParamCmd.CHRID,  
+    ParamCmd.DECFMT,    
+    ParamCmd.SFLENDTXT, 
+    ParamCmd.WAITFILE,  
+    ParamCmd.WAITRCD,   
+    ParamCmd.DTAQ,      
+    ParamCmd.SHARE, 
+    ParamCmd.SRTSEQ,
+    ParamCmd.LANGID,  
+    ParamCmd.LVLCHK,  
+    ParamCmd.AUT,     
+    ParamCmd.REPLACE
+  );
+
+  // CRTPF
+  public static final List<ParamCmd> ddsPfPattern = Arrays.asList(
+    ParamCmd.FILE,
+    ParamCmd.SRCFILE,
+    ParamCmd.SRCMBR,  
+    ParamCmd.RCDLEN,  
+    ParamCmd.GENLVL,  
+    ParamCmd.FLAG,    
+    ParamCmd.FILETYPE,
+    ParamCmd.MBR,     
+    ParamCmd.TEXT,    
+    ParamCmd.OPTION,
+    ParamCmd.SYSTEM,   
+    ParamCmd.EXPDATE,  
+    ParamCmd.MAXMBRS,  
+    ParamCmd.ACCPTHSIZ,
+    ParamCmd.PAGESIZE, 
+    ParamCmd.MAINT,    
+    ParamCmd.RECOVER,  
+    ParamCmd.FRCACCPTH,
+    ParamCmd.SIZE,
+    ParamCmd.ALLOCATE,
+    ParamCmd.CONTIG,  
+    ParamCmd.UNIT,    
+    ParamCmd.FRCRATIO,
+    ParamCmd.WAITFILE,
+    ParamCmd.WAITRCD, 
+    ParamCmd.SHARE,   
+    ParamCmd.DLTPCT,  
+    ParamCmd.REUSEDLT,
+    ParamCmd.SRTSEQ,       
+    ParamCmd.LANGID,  
+    ParamCmd.CCSID, 
+    ParamCmd.ALWUPD,
+    ParamCmd.ALWDLT,
+    ParamCmd.LVLCHK,
+    ParamCmd.NODGRP,
+    ParamCmd.PTNKEY,
+    ParamCmd.AUT
+  );
+
+  // CRTLF
+  public static final List<ParamCmd> ddsLfPattern = Arrays.asList(
+    ParamCmd.FILE,
+    ParamCmd.SRCFILE,
+    ParamCmd.SRCMBR,  
+    ParamCmd.GENLVL,  
+    ParamCmd.FLAG,    
+    ParamCmd.FILETYPE,
+    ParamCmd.MBR,     
+    ParamCmd.DTAMBRS, 
+    ParamCmd.TEXT,
+    ParamCmd.OPTION,
+    ParamCmd.SYSTEM,    
+    ParamCmd.MAXMBRS,   
+    ParamCmd.ACCPTHSIZ, 
+    ParamCmd.PAGESIZE,  
+    ParamCmd.MAINT,     
+    ParamCmd.RECOVER,   
+    ParamCmd.FRCACCPTH, 
+    ParamCmd.UNIT,      
+    ParamCmd.FMTSLR,
+    ParamCmd.FRCRATIO, 
+    ParamCmd.WAITFILE, 
+    ParamCmd.WAITRCD,  
+    ParamCmd.SHARE,    
+    ParamCmd.SRTSEQ,   
+    ParamCmd.LANGID,   
+    ParamCmd.LVLCHK,   
+    ParamCmd.AUT 
+  );
+
+  // CRTPRTF
+  public static final List<ParamCmd> ddsPrtfPattern = Arrays.asList(
+    ParamCmd.FILE,   
+    ParamCmd.SRCFILE,
+    ParamCmd.SRCMBR, 
+    ParamCmd.GENLVL, 
+    ParamCmd.FLAG,   
+    ParamCmd.DEV,    
+    ParamCmd.DEVTYPE,
+    ParamCmd.TEXT,   
+    ParamCmd.OPTION,
+    ParamCmd.PAGESIZE,
+    ParamCmd.LPI,     
+    ParamCmd.CPI,     
+    ParamCmd.FRONTMGN,
+    ParamCmd.BACKMGN,
+    ParamCmd.OVRFLW,  
+    ParamCmd.FOLD,    
+    ParamCmd.RPLUNPRT,
+    ParamCmd.ALIGN,   
+    ParamCmd.CTLCHAR, 
+    ParamCmd.CHLVAL,  
+    ParamCmd.FIDELITY,
+    ParamCmd.PRTQLTY,  
+    ParamCmd.FORMFEED, 
+    ParamCmd.DRAWER,   
+    ParamCmd.OUTBIN,   
+    ParamCmd.FONT,     
+    ParamCmd.CHRID,    
+    ParamCmd.DECFMT,   
+    ParamCmd.FNTCHRSET,
+    ParamCmd.CDEFNT,
+    ParamCmd.TBLREFCHR,
+    ParamCmd.PAGDFN,   
+    ParamCmd.FORMDF,   
+    ParamCmd.AFPCHARS,
+    ParamCmd.PAGRTT,   
+    ParamCmd.MULTIUP,  
+    ParamCmd.REDUCE,   
+    ParamCmd.PRTTXT,   
+    ParamCmd.JUSTIFY,  
+    ParamCmd.DUPLEX,   
+    ParamCmd.UOM,      
+    ParamCmd.FRONTOVL, 
+    ParamCmd.BACKOVL,
+    ParamCmd.CVTLINDTA, 
+    ParamCmd.IPDSPASTHR,
+    ParamCmd.USRRSCLIBL,
+    ParamCmd.CORNERSTPL,
+    ParamCmd.EDGESTITCH,
+    ParamCmd.SADLSTITCH,
+    ParamCmd.FNTRSL,
+    ParamCmd.DFRWRT,
+    ParamCmd.SPOOL, 
+    ParamCmd.OUTQ,  
+    ParamCmd.FORMTYPE, 
+    ParamCmd.COPIES,   
+    ParamCmd.EXPDATE,  
+    ParamCmd.DAYS,     
+    ParamCmd.PAGERANGE,
+    ParamCmd.MAXRCDS, 
+    ParamCmd.FILESEP, 
+    ParamCmd.SCHEDULE,
+    ParamCmd.HOLD,    
+    ParamCmd.SAVE,    
+    ParamCmd.OUTPTY,  
+    ParamCmd.USRDTA,  
+    ParamCmd.SPLFOWN, 
+    ParamCmd.USRDFNOPT,
+    ParamCmd.USRDFNDTA,
+    ParamCmd.USRDFNOBJ,
+    ParamCmd.TOSTMF,   
+    ParamCmd.WSCST,    
+    ParamCmd.WAITFILE, 
+    ParamCmd.SHARE,   
+    ParamCmd.LVLCHK,   
+    ParamCmd.AUT,      
+    ParamCmd.REPLACE
+  );
+
+  // CRTCMD
+  public static final List<ParamCmd> CmdPattern = Arrays.asList(
+    ParamCmd.CMD,
+    ParamCmd.PGM,        
+    ParamCmd.SRCFILE,    
+    ParamCmd.SRCMBR,     
+    ParamCmd.SRCSTMF,    
+    ParamCmd.REXSRCFILE, 
+    ParamCmd.REXSRCMBR,  
+    ParamCmd.REXCMDENV,  
+    ParamCmd.REXEXITPGM,
+    ParamCmd.THDSAFE
+  );
+
+  //TODO: Add config file support like YAML. This will allow specific patterns to be provided.
+  // So, you would try to load the file with the param patter or use the default one.
+
+  /* Maps compilation command to its pattern */
+  public static final Map<CompCmd, List<ParamCmd>> cmdToPatternMap = new EnumMap<>(CompCmd.class);
+
+  static{
+    /* ILE */
+    cmdToPatternMap.put(CompCmd.CRTSRVPGM, SrvpgmPattern);
+    cmdToPatternMap.put(CompCmd.CRTBNDRPG, ileRpgPgmPattern);
+    cmdToPatternMap.put(CompCmd.CRTBNDCL, ileClPgmPattern);
+    cmdToPatternMap.put(CompCmd.CRTRPGMOD, RpgModulePattern);
+    cmdToPatternMap.put(CompCmd.CRTCLMOD, ClleModulePattern);
+    cmdToPatternMap.put(CompCmd.CRTSQLRPGI, SqlRpgPgmPattern);
+    /* OPM */
+    cmdToPatternMap.put(CompCmd.CRTRPGPGM, opmRpgPgmPattern);
+    cmdToPatternMap.put(CompCmd.CRTCLPGM, opmClPgmPattern);
+    /* SQL */
+    cmdToPatternMap.put(CompCmd.RUNSQLSTM, SqlPattern);
+    /* DDS */
+    cmdToPatternMap.put(CompCmd.CRTDSPF, ddsDspfPattern);
+    cmdToPatternMap.put(CompCmd.CRTPF, ddsPfPattern);
+    cmdToPatternMap.put(CompCmd.CRTLF, ddsLfPattern);
+
+    cmdToPatternMap.put(CompCmd.CRTPRTF, ddsPrtfPattern);
+    /* CMD */
+    cmdToPatternMap.put(CompCmd.CRTCMD, CmdPattern);
+  }     
 
   public static CompCmd getCompilationCommand(SourceType sourceType, ObjectType objectType){
     return typeToCmdMap.get(sourceType).get(objectType);
